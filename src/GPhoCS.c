d0a994ab9e9f28f2200ec46e2f56513b7913e4db
selotape <RonVisbord@Gmail.com>
2017-04-13 17:06:34 +0300

    fixed trace file indentation problem

--- src/GPhoCS.c
+++ src/GPhoCS.c
@@ -1556,7 +1556,7 @@ int performMCMC() {
 				if (iteration >= 0 && iteration % (mcmcSetup.sampleSkip + 1) == 0) {
 						fprintf(ioSetup.traceFile, "%d\t", iteration);
 						printParamVals(paramVals, 0, mcmcSetup.numParameters,	ioSetup.traceFile);
-						fprintf(ioSetup.traceFile, "\t%.6f\t%.6f\t%.6f\n", dataState.logLikelihood, dataState.dataLogLikelihood, dataState.genealogyLogLikelihood);
+						fprintf(ioSetup.traceFile, "%.6f\t%.6f\t%.6f\n", dataState.logLikelihood, dataState.dataLogLikelihood, dataState.genealogyLogLikelihood);
 						fflush(ioSetup.traceFile);
 
 						if (recordCoalStats  && 0) {
