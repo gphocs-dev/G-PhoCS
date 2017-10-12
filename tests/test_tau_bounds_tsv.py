ba703b0714cbf2f0158ee374b50bf55a8d73ac97
ron <ronvisbord@gmail.com>
2017-10-12 23:26:50 +0300

    implemented simple python test to assert tau_bounds results

---
+++ tests/test_tau_bounds_tsv.py
@@ -0,0 +1,27 @@
+import csv
+
+try:
+    import pytest
+except:
+    pass
+
+TAU_BOUNDS_TSV_FILE = 'out/sample-tau-bounds.tsv'
+
+
+def test_taus_lower_then_bounds():
+    with open(TAU_BOUNDS_TSV_FILE) as tau_bounds_file:
+        tau_bounds = csv.reader(tau_bounds_file, delimiter='\t')
+        next(tau_bounds)  # skip header
+        for i, row in enumerate(tau_bounds):
+            for j, (bound, tau) in enumerate(chunks(row[1:], 2)):
+                assert bound >= tau, "bad bound in row %d in pair %d" % (i + 2, j)
+
+
+def chunks(l, n):
+    """Yield successive n-sized chunks from l."""
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
+
+
+if __name__ == '__main__':
+    pytest.main()
