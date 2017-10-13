c9a3d5f5202545fb16368cc95cce6ac0bd0fb1a1
ron <ronvisbord@gmail.com>
2017-10-13 12:59:48 +0300

    start implementing runtime tau bound assertions

--- src/TauBounds.cpp
+++ src/TauBounds.cpp
@@ -4,11 +4,16 @@
 #include "patch.h"
 #include "GPhoCS.h"
 #include "McRefCommon.h"
-#include "MemoryMng.h"
 
 double *tau_bounds;
 int *lca_pops;
 
+
+void assertBoundsAreMonotonousAscending();
+
+void runTauBoundsAssertions();
+
+void assertTausAreSmallerThanBounds();
 
 void calculateTauBounds() {
   initializeTauBounds();
@@ -17,6 +22,20 @@ void calculateTauBounds() {
     int rootNodeId = dataState.lociData[gen]->root;
     calculateLociTauBounds(rootNodeId, gen);
   }
+  if (DEBUG_TAU_BOUNDS) runTauBoundsAssertions();
+}
+
+void runTauBoundsAssertions() {
+  assertBoundsAreMonotonousAscending();
+  assertTausAreSmallerThanBounds();
+}
+
+void assertTausAreSmallerThanBounds() {
+
+}
+
+void assertBoundsAreMonotonousAscending() {
+
 }
 
 void calculateLociTauBounds(int nodeId, int gen) {
@@ -31,49 +50,32 @@ void calculateLociTauBounds(int nodeId, int gen) {
 
   lca_pops[nodeId] = lca(lca_pops[currentNode->leftSon], lca_pops[currentNode->rightSon]);
 
-  updateTauBoundsOfDescendants(lca_pops[nodeId], currentNode->age, nodeId, gen);
+  updateTauBoundsOfDescendants(lca_pops[nodeId], currentNode->age);
 }
 
-void updateTauBoundsOfDescendants(int pop, double bound, int nodeId, int gen) { // nocommit horrible signature for debug
-  if (isLeaf(pop)) return;
-
-  if (bound < dataSetup.popTree->pops[pop]->age) {
-    //nocommit
-    printf("\nerror in tau_bounds:\n%.10f is the bound but\n%.10f is population %ss tau\n", bound,
-           dataSetup.popTree->pops[pop]->age, getPopName(pop));
-    printf("just FYI, the bounding node %d lives in pop %s", nodeId, getPopName(nodePops[gen][nodeId]));
-    exit(-1);
-  }
+void updateTauBoundsOfDescendants(int pop, double bound) {
+  if (isLeafPop(pop)) return;
 
   tau_bounds[pop] = fmin(tau_bounds[pop], bound);
 
-  updateTauBoundsOfDescendants(getSon(pop, LEFT), bound, nodeId, gen);
-  updateTauBoundsOfDescendants(getSon(pop, RIGHT), bound, nodeId, gen);
+  updateTauBoundsOfDescendants(getSon(pop, LEFT), bound);
+  updateTauBoundsOfDescendants(getSon(pop, RIGHT), bound);
 }
-
-void initializeLcaPops(int gen) {
-  for (int nodeId = 0; nodeId < numNodes(); nodeId++) {
-    LikelihoodNode *currentNode = getNode(nodeId, gen);
-    if (isLeafNode(currentNode)) {
-      lca_pops[nodeId] = getLeafNodePop(nodeId, gen);
-    }
-  }
-}
-
 
 void printTauBoundsHeader(FILE *file) {
   fprintf(file, "iteration");
   for (int pop = 0; pop < dataSetup.popTree->numPops; pop++) {
-    if (isLeaf(pop)) continue;
+    if (isLeafPop(pop)) continue;
     fprintf(file, "\t%s_bound\t%s_tau", getPopName(pop), getPopName(pop));
   }
   fprintf(file, "\n");
 }
 
+
 void printTauBounds(int iteration, FILE *file) {
   fprintf(file, "%d", iteration);
   for (int pop = 0; pop < dataSetup.popTree->numPops; pop++) {
-    if (isLeaf(pop)) continue;
+    if (isLeafPop(pop)) continue;
     fprintf(file, "\t%.40f\t%.40f", tau_bounds[pop], dataSetup.popTree->pops[pop]->age);
   }
   fprintf(file, "\n");
@@ -88,8 +90,17 @@ void allocateTauBoundsMem() {
 
 void initializeTauBounds() {
   for (int pop = 0; pop < dataSetup.popTree->numPops; pop++) {
-    if (!isLeaf(pop)) {
+    if (!isLeafPop(pop)) {
       tau_bounds[pop] = 100.0; // TODO - init with proper value (tau of father pop? MAX/INF value?)
+    }
+  }
+}
+
+void initializeLcaPops(int gen) {
+  for (int nodeId = 0; nodeId < numNodes(); nodeId++) {
+    LikelihoodNode *currentNode = getNode(nodeId, gen);
+    if (isLeafNode(currentNode)) {
+      lca_pops[nodeId] = getNodePop(nodeId, gen);
     }
   }
 }
