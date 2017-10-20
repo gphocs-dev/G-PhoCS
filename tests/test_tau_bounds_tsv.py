42c99d59798f94940e01a4b7a16a828516a825ad
ron <ronvisbord@gmail.com>
2017-10-20 11:53:46 +0300

    wrote tau-bounds diff test

--- tests/test_tau_bounds_tsv.py
+++ tests/test_tau_bounds_tsv.py
@@ -10,6 +10,12 @@ TAU_BOUNDS_TSV_FILE = 'out/sample-tau-bounds.tsv'
 tau_bounds_file = open(TAU_BOUNDS_TSV_FILE)
 
 
+def chunks(l, n):
+    """Yield successive n-sized chunks from l."""
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
+
+
 def test_taus_lower_then_bounds():
     tau_bounds = csv.reader(tau_bounds_file, delimiter='\t')
     next(tau_bounds)  # skip header
@@ -20,10 +26,6 @@ def test_taus_lower_then_bounds():
 
 
 
-def chunks(l, n):
-    """Yield successive n-sized chunks from l."""
-    for i in range(0, len(l), n):
-        yield l[i:i + n]
 
 
 if __name__ == '__main__':
